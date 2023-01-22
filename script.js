d53a7c9e98e1ab9eec01c2361bdb9348551be233
Gustavo Cardoso <cargugati@gmail.com>
2023-01-21 23:38:41 -0300

    inicial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso 🔴")
+    return
+  }
+
+  alert("Adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  window.localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+// const data = {
+//   run: ["01-10"],
+//   takePills: ["01-13"],
+// }
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
